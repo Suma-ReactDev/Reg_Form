7ad987a5cf923de5c66fe9b487261150135954ea
Suma-ReactDev <k.sumakumari@gmail.com>
2022-09-29 17:43:12 +0530

    Testing Date component

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -5,8 +5,6 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useFormik, useFormikContext, useField } from 'formik';
 const DatePickerField = (props) => {
   const {label, name, ...rest} = props
-  const { setFieldValue } = useFormikContext();
-  const [field] = useField(props);
   return (
     <div className='ml-10 mr-10'>
       <label htmlFor={name} className='block text-gray-500 font-bold mb-1'>{name}</label>
@@ -14,17 +12,18 @@ const DatePickerField = (props) => {
       className="border-2 border-gray-200 rounded w-full py-2 
       px-4 text-gray-700 leading-tight focus:outline-none 
       focus:bg-white focus:border-purple-500">
-        {()=>{
+        {({ field, meta, form: { setFieldValue } }) => {
           console.log(field)
-          return <DatePicker
-          {...field}
-          {...props}
-          selected={(field.value && new Date(field.value)) || null}
-          onChange={(val) => {
-            setFieldValue(field.name, val);
-          }}
-        />
-        }}
+        return (
+          <DatePicker
+            {...field}
+            selected={field.value || null}
+            onChange={(val) => {
+              setFieldValue(field.name, val);
+            }}
+          />
+        );
+      }}
       </Field>
       <div className='text-lg text-red-600 font-medium'><ErrorMessage name={name} /></div>
     </div>
