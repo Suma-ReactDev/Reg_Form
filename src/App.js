a1ca77904357d5a6137bd24bce85a91311f1cc20
Suma-ReactDev <k.sumakumari@gmail.com>
2022-10-07 17:22:59 +0530

    CRUD done

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import {RegForm, Registered, FormikContainer, DateComponent, FormikContainerEdit
 from './components/fileImports'
 import { Routes, Route } from "react-router-dom";
 
-import UserList from "./components/UserList";
-import Registered from "./components/Registered";
-import { Routes, Route } from "react-router-dom";
-import EditUser from "./components/EditUser";
-import FormikContainer from "./components/FormikContainer";
-import DateComponent from "./components/DateComponent";
-import FormikContainerEdit from "./components/FormikContainerEdit";
 function App() {
   return (
     <div
       className="flex flex-col items-center justify-center
     min-h-screen mx-auto bg-slate-400 rounded"
     >
-      {/* <UserList /> */}
+  
       <Routes>
         <Route path="/" element={<Registered />} />
         <Route path="rgster" element={<FormikContainer />} />
-        <Route path="date" element={<DateComponent />} />
-        <Route path="edit-user" element={<EditUser />} />
+        {/* <Route path="date" element={<DateComponent />} /> */}
         <Route path="rgster-edit" element={<FormikContainerEdit />} />
       </Routes>
     </div>
