80086987c65060408ee9f2e5ec8e06c38541e02a
Suma-ReactDev <k.sumakumari@gmail.com>
2022-10-11 10:17:11 +0530

    CRUD done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ContextProvider } from "./components/store/user-context";
+import { ContextProvider } from "./components/store/usercontext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ContextProvider>
